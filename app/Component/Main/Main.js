7d480d53dd63b81ec64b05cff5d6406bd7841f72
Robert Zhang <robertsky.zhang@gmail.com>
2016-07-08 00:46:09 -0400

    updated for mdl handling

--- app/Component/Main/Main.js
+++ app/Component/Main/Main.js
@@ -12,17 +12,23 @@ export default class Main extends React.Component {
 		super();
 	}
 
-	render(){
-	 return (
-			 <div className="mdl-layout mdl-js-layout">
-			 	<Header/>
-				 <main className="mdl-layout__content">
-					 <div className="page-content">
-						 {this.props.children}
-					 </div>
-				 </main>
-			 	<Footer/>
-			 </div>
-	 );
+	render() {
+		return (
+				<div className="mdl-layout mdl-js-layout">
+					<Header/>
+					<main className="mdl-layout__content">
+						<div className="page-content">
+							{this.props.children}
+						</div>
+					</main>
+					<Footer/>
+				</div>
+		);
+	}
+
+	componentDidUpdate() {
+		// This upgrades all upgradable components (i.e. with 'mdl-js-*' class)
+		componentHandler.upgradeDom();
+
 	}
 }
